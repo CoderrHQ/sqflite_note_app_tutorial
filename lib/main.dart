27fa97a40352bc59656f02d75ad48d51d21c90cb
Code HQ <codeheadq@gmail.com>
2024-03-26 07:36:07 +0430

    final code for the sqflite todo app tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_note_app_tutorial/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
